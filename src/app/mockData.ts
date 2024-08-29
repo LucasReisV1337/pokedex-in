73ae06fcfdbeb4e1c9545a610eb28924ef187d12
Lucas Reis <l.vasconcelos@ufba.br>
2024-08-29 01:02:56 -0300

    feat: :sparkles: created the minimal frontend without connection to the api

---
+++ src/app/mockData.ts
@@ -0,0 +1,11 @@
+// mockData.ts
+export type AbilitiesMap = {
+    [key: string]: string[];
+  };
+  
+  export const mockAbilities: AbilitiesMap = {
+    pikachu: ['Thunder Shock', 'Quick Attack', 'Iron Tail', 'Thunderbolt'],
+    bulbasaur: ['Vine Whip', 'Growl', 'Leech Seed', 'Razor Leaf'],
+    charmander: ['Scratch', 'Growl', 'Ember', 'Leer'],
+  };
+  
